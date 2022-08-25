688a2395cc2de672b379e4d0d0c196c6a336f898
mw <marcel.wurm@mailbox.org>
2022-08-25 11:27:25 +0200

    Set fish shell variable and ssh-agent start

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,10 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    export PATH="$HOME/.cargo/bin:$PATH"
+  # Commands to run in interactive sessions can go here
+  export PATH="$HOME/.cargo/bin:$PATH"
+
+  # fish_ssh_agent
+  begin
+    set -lx SHELL $__fish_bin_dir/fish
+    eval (keychain --eval --agents ssh -Q --quiet ~/.ssh/github_sticky --nogui --noask)
+  end
 end
