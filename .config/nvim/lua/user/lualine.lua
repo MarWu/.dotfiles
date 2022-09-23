d280f04faf49ed21f2f248758c2f6bd55d4958d4
mw <marcel.wurm@mailbox.org>
2022-09-23 11:26:15 +0200

    Added lualine-lsp-progress

--- .config/nvim/lua/user/lualine.lua
+++ .config/nvim/lua/user/lualine.lua
@@ -50,7 +50,7 @@ lualine.setup {
   sections = {
     lualine_a = { "mode" },
     lualine_b = {"branch"},
-    lualine_c = { diagnostics },
+    lualine_c = { diagnostics, "lsp_progress" },
     lualine_x = { diff, spaces, "encoding", filetype },
     lualine_y = { location },
     lualine_z = { "progress" },
