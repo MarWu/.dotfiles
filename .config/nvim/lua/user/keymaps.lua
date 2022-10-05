1a590efa743317f3440c27855f5ab031539f8057
mw <marcel.wurm@mailbox.org>
2022-10-05 14:18:41 +0200

    Added plugin: OpenInGH

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -79,3 +79,7 @@ keymap("n", "<leader>dr", "<cmd>lua require'dap'.repl.toggle()<cr>", opts)
 keymap("n", "<leader>dl", "<cmd>lua require'dap'.run_last()<cr>", opts)
 keymap("n", "<leader>du", "<cmd>lua require'dapui'.toggle()<cr>", opts)
 keymap("n", "<leader>dt", "<cmd>lua require'dap'.terminate()<cr>", opts)
+
+-- OpenInGH
+vim.api.nvim_set_keymap("n", "<leader>gr", ":OpenInGHRepo<CR>", { silent = true, noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>gf", ":OpenInGHFile<CR>", { silent = true, noremap = true })
